13b445102cfec013467e48ca0ffbf877176f1595
armando barra p <armando.barra.p@gmail.com>
2024-11-08 18:56:44 -0300

    Create router conf in main and create a base from home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/web/home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+   // element: <Root />, //TODO: add layout component here
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      // for create a new route only add a new object like this:
+      /*  {
+        path: "team",
+        element: <Home />,
+      }*/
 
+    ],
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
