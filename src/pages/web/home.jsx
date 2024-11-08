90a966d9eee26f83521a9ea6f439821986f1e422
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-08 20:35:34 -0300

    Added conditional render

--- src/pages/web/home.jsx
+++ src/pages/web/home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+//import React from "react";
+import { useState, useEffect } from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid2';
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, "3000");
+    });
+
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to the Home page</p>
-            <p> Hola toketon</p> 
-        </div>
+        <Grid sx={{paddingTop: '50px', marginLeft:'30px', display: 'flex', flexDirection: 'column', height: '100vh', width: '100%' }} size={12}>
+            {isLoading ?
+                <>
+                    <CircularProgress/>
+                </>
+                :
+                <>
+                    <h1>Home</h1>
+                    <p>Welcome to the Home page</p>
+                    <p> Hola xanoto</p>
+                </>
+            }
+        </Grid>
     );
-    };
+};
 export default Home;
